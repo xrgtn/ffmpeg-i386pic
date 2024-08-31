1c6b9a3e8e39009ded6ca656e59a7739629a7612
Alexander Gattin <xrgtn@yandex.ru>
2024-08-31 00:24:24 +0000

    add i386 PIC support to h264_qpel_8bit.asm
    Use no-save PIC with not-yet-initialized register in
    %1_h264_qpel4_h_lowpass() and %1_h264_qpel4_h_lowpass_l2() functions.
    
    Unblock rpicsave and use push/pop PIC in the rest.

--- libavcodec/x86/h264_qpel_8bit.asm
+++ libavcodec/x86/h264_qpel_8bit.asm
@@ -58,8 +58,11 @@ cglobal %1_h264_qpel4_h_lowpass, 4,5 ; dst, src, dstStride, srcStride
     movsxdifnidn  r2, r2d
     movsxdifnidn  r3, r3d
     pxor          m7, m7
-    mova          m4, [pw_5]
-    mova          m5, [pw_16]
+    PIC_BEGIN r4d, 0 ; r4d not initialized yet
+    CHECK_REG_COLLISION "rpic","r0","r1","r2","r3"
+    mova          m4, [pic(pw_5)]
+    mova          m5, [pic(pw_16)]
+    PIC_END ; r4d, no-save
     mov          r4d, 4
 .loop:
     movh          m1, [r1-1]
@@ -98,11 +101,14 @@ QPEL4_H_LOWPASS_OP avg
 
 %macro QPEL8_H_LOWPASS_OP 1
 cglobal %1_h264_qpel8_h_lowpass, 4,5 ; dst, src, dstStride, srcStride
+    %define rpicsave ; safe to push/pop rpic
+    PIC_BEGIN r5
+    CHECK_REG_COLLISION "rpic","r0","r1","r2","r3","r4d"
     movsxdifnidn  r2, r2d
     movsxdifnidn  r3, r3d
     mov          r4d, 8
     pxor          m7, m7
-    mova          m6, [pw_5]
+    mova          m6, [pic(pw_5)]
 .loop:
     mova          m0, [r1]
     mova          m2, [r1+1]
@@ -136,7 +142,7 @@ cglobal %1_h264_qpel8_h_lowpass, 4,5 ; dst, src, dstStride, srcStride
     punpcklbw     m5, m7
     paddw         m2, m3
     paddw         m4, m5
-    mova          m5, [pw_16]
+    mova          m5, [pic(pw_16)]
     paddw         m2, m5
     paddw         m4, m5
     paddw         m0, m2
@@ -149,6 +155,7 @@ cglobal %1_h264_qpel8_h_lowpass, 4,5 ; dst, src, dstStride, srcStride
     add           r1, r3
     dec          r4d
     jg         .loop
+    PIC_END ; r5, push/pop
     RET
 %endmacro
 
@@ -158,11 +165,14 @@ QPEL8_H_LOWPASS_OP avg
 
 %macro QPEL8_H_LOWPASS_OP_XMM 1
 cglobal %1_h264_qpel8_h_lowpass, 4,5,8 ; dst, src, dstStride, srcStride
+    %define rpicsave ; safe to push/pop rpic
+    PIC_BEGIN r5
+    CHECK_REG_COLLISION "rpic","r0","r1","r2","r3","r4d"
     movsxdifnidn  r2, r2d
     movsxdifnidn  r3, r3d
     mov          r4d, 8
     pxor          m7, m7
-    mova          m6, [pw_5]
+    mova          m6, [pic(pw_5)]
 .loop:
     movu          m1, [r1-2]
     mova          m0, m1
@@ -182,7 +192,7 @@ cglobal %1_h264_qpel8_h_lowpass, 4,5,8 ; dst, src, dstStride, srcStride
     paddw         m1, m4
     psllw         m2, 2
     psubw         m2, m1
-    paddw         m0, [pw_16]
+    paddw         m0, [pic(pw_16)]
     pmullw        m2, m6
     paddw         m2, m0
     psraw         m2, 5
@@ -192,6 +202,7 @@ cglobal %1_h264_qpel8_h_lowpass, 4,5,8 ; dst, src, dstStride, srcStride
     add           r0, r2
     dec          r4d
     jne        .loop
+    PIC_END ; r5, push/pop
     RET
 %endmacro
 
@@ -202,11 +213,14 @@ QPEL8_H_LOWPASS_OP_XMM avg
 
 %macro QPEL4_H_LOWPASS_L2_OP 1
 cglobal %1_h264_qpel4_h_lowpass_l2, 5,6 ; dst, src, src2, dstStride, srcStride
+    PIC_BEGIN r5d, 0 ; r5d not initialized yet
+    CHECK_REG_COLLISION "rpic","r0","r1","r2","r3","r4"
     movsxdifnidn  r3, r3d
     movsxdifnidn  r4, r4d
     pxor          m7, m7
-    mova          m4, [pw_5]
-    mova          m5, [pw_16]
+    mova          m4, [pic(pw_5)]
+    mova          m5, [pic(pw_16)]
+    PIC_END ; r5d, no-save
     mov          r5d, 4
 .loop:
     movh          m1, [r1-1]
@@ -249,11 +263,14 @@ QPEL4_H_LOWPASS_L2_OP avg
 
 %macro QPEL8_H_LOWPASS_L2_OP 1
 cglobal %1_h264_qpel8_h_lowpass_l2, 5,6 ; dst, src, src2, dstStride, srcStride
+    %define rpicsave ; safe to push/pop rpic
+    PIC_BEGIN r6
+    CHECK_REG_COLLISION "rpic","r0","r1","r2","r3","r4","r5d"
     movsxdifnidn  r3, r3d
     movsxdifnidn  r4, r4d
     mov          r5d, 8
     pxor          m7, m7
-    mova          m6, [pw_5]
+    mova          m6, [pic(pw_5)]
 .loop:
     mova          m0, [r1]
     mova          m2, [r1+1]
@@ -287,7 +304,7 @@ cglobal %1_h264_qpel8_h_lowpass_l2, 5,6 ; dst, src, src2, dstStride, srcStride
     punpcklbw     m5, m7
     paddw         m2, m3
     paddw         m4, m5
-    mova          m5, [pw_16]
+    mova          m5, [pic(pw_16)]
     paddw         m2, m5
     paddw         m4, m5
     paddw         m0, m2
@@ -303,6 +320,7 @@ cglobal %1_h264_qpel8_h_lowpass_l2, 5,6 ; dst, src, src2, dstStride, srcStride
     add           r2, r4
     dec          r5d
     jg         .loop
+    PIC_END ; r6, push/pop
     RET
 %endmacro
 
@@ -313,11 +331,14 @@ QPEL8_H_LOWPASS_L2_OP avg
 
 %macro QPEL8_H_LOWPASS_L2_OP_XMM 1
 cglobal %1_h264_qpel8_h_lowpass_l2, 5,6,8 ; dst, src, src2, dstStride, src2Stride
+    %define rpicsave ; safe to push/pop rpic
+    PIC_BEGIN r6
+    CHECK_REG_COLLISION "rpic","r0","r1","r2","r3","r4","r5d"
     movsxdifnidn  r3, r3d
     movsxdifnidn  r4, r4d
     mov          r5d, 8
     pxor          m7, m7
-    mova          m6, [pw_5]
+    mova          m6, [pic(pw_5)]
 .loop:
     lddqu         m1, [r1-2]
     mova          m0, m1
@@ -338,7 +359,7 @@ cglobal %1_h264_qpel8_h_lowpass_l2, 5,6,8 ; dst, src, src2, dstStride, src2Strid
     psllw         m2, 2
     movh          m3, [r2]
     psubw         m2, m1
-    paddw         m0, [pw_16]
+    paddw         m0, [pic(pw_16)]
     pmullw        m2, m6
     paddw         m2, m0
     psraw         m2, 5
@@ -350,6 +371,7 @@ cglobal %1_h264_qpel8_h_lowpass_l2, 5,6,8 ; dst, src, src2, dstStride, src2Strid
     add           r2, r4
     dec          r5d
     jg         .loop
+    PIC_END ; r6, push/pop
     RET
 %endmacro
 
@@ -360,7 +382,7 @@ QPEL8_H_LOWPASS_L2_OP_XMM avg
 
 ; All functions that call this are required to have function arguments of
 ; dst, src, dstStride, srcStride
-%macro FILT_V 1
+%macro FILT_V 1 ; r0..3; PIC
     mova      m6, m2
     movh      m5, [r1]
     paddw     m6, m3
@@ -368,8 +390,11 @@ QPEL8_H_LOWPASS_L2_OP_XMM avg
     psubw     m6, m1
     psubw     m6, m4
     punpcklbw m5, m7
-    pmullw    m6, [pw_5]
-    paddw     m0, [pw_16]
+    CHECK_REG_COLLISION "rpic","r0","r1","r2","r3"
+    PIC_BEGIN r4
+    pmullw    m6, [pic(pw_5)]
+    paddw     m0, [pic(pw_16)]
+    PIC_END
     add       r1, r3
     paddw     m0, m5
     paddw     m6, m0
@@ -382,6 +407,9 @@ QPEL8_H_LOWPASS_L2_OP_XMM avg
 
 %macro QPEL4_V_LOWPASS_OP 1
 cglobal %1_h264_qpel4_v_lowpass, 4,4 ; dst, src, dstStride, srcStride
+    %define rpicsave ; safe to push/pop rpic
+    PIC_BEGIN r4
+    CHECK_REG_COLLISION "rpic","r0","r1","r2","r3"
     movsxdifnidn  r2, r2d
     movsxdifnidn  r3, r3d
     sub           r1, r3
@@ -400,10 +428,11 @@ cglobal %1_h264_qpel4_v_lowpass, 4,4 ; dst, src, dstStride, srcStride
     punpcklbw     m2, m7
     punpcklbw     m3, m7
     punpcklbw     m4, m7
-    FILT_V        %1
-    FILT_V        %1
-    FILT_V        %1
-    FILT_V        %1
+    FILT_V        %1 ; r0..3; PIC
+    FILT_V        %1 ; r0..3; PIC
+    FILT_V        %1 ; r0..3; PIC
+    FILT_V        %1 ; r0..3; PIC
+    PIC_END ; r4, push/pop
     RET
 %endmacro
 
@@ -425,6 +454,9 @@ cglobal %1_h264_qpel8or16_v_lowpass_op, 5,5,8 ; dst, src, dstStride, srcStride, 
     movsxdifnidn  r2, r2d
     movsxdifnidn  r3, r3d
 %endif
+    %define rpicsave ; safe to push/pop rpic
+    PIC_BEGIN r5
+    CHECK_REG_COLLISION "rpic","r0","r1","r2","r3","r4d"
     pxor          m7, m7
     movh          m0, [r1]
     movh          m1, [r1+r3]
@@ -439,7 +471,7 @@ cglobal %1_h264_qpel8or16_v_lowpass_op, 5,5,8 ; dst, src, dstStride, srcStride, 
     punpcklbw     m2, m7
     punpcklbw     m3, m7
     punpcklbw     m4, m7
-    FILT_V        %1
+    FILT_V        %1 ; r0..3; PIC
     FILT_V        %1
     FILT_V        %1
     FILT_V        %1
@@ -456,8 +488,9 @@ cglobal %1_h264_qpel8or16_v_lowpass_op, 5,5,8 ; dst, src, dstStride, srcStride, 
     FILT_V        %1
     FILT_V        %1
     FILT_V        %1
-    FILT_V        %1
+    FILT_V        %1 ; r0..3; PIC
 .end:
+    PIC_END ; r5, push/pop
     RET
 %endmacro
 
@@ -468,16 +501,19 @@ QPEL8OR16_V_LOWPASS_OP avg
 
 ; All functions that use this are required to have args:
 ; src, tmp, srcSize
-%macro FILT_HV 1 ; offset
+%macro FILT_HV 1 ; offset ; r0..2; PIC
     mova           m6, m2
     movh           m5, [r0]
     paddw          m6, m3
     psllw          m6, 2
-    paddw          m0, [pw_16]
+    CHECK_REG_COLLISION "rpic",%1,"r0","r1","r2"
+    PIC_BEGIN r4
+    paddw          m0, [pic(pw_16)]
     psubw          m6, m1
     psubw          m6, m4
     punpcklbw      m5, m7
-    pmullw         m6, [pw_5]
+    pmullw         m6, [pic(pw_5)]
+    PIC_END
     paddw          m0, m5
     add            r0, r2
     paddw          m6, m0
@@ -487,6 +523,9 @@ QPEL8OR16_V_LOWPASS_OP avg
 
 %macro QPEL4_HV1_LOWPASS_OP 1
 cglobal %1_h264_qpel4_hv_lowpass_v, 3,3 ; src, tmp, srcStride
+    %define rpicsave ; safe to push/pop rpic
+    PIC_BEGIN r3
+    CHECK_REG_COLLISION "rpic","r0","r1","r2"
     movsxdifnidn  r2, r2d
     pxor          m7, m7
     movh          m0, [r0]
@@ -502,10 +541,11 @@ cglobal %1_h264_qpel4_hv_lowpass_v, 3,3 ; src, tmp, srcStride
     punpcklbw     m2, m7
     punpcklbw     m3, m7
     punpcklbw     m4, m7
-    FILT_HV       0*24
+    FILT_HV       0*24 ; r0..2; PIC
     FILT_HV       1*24
     FILT_HV       2*24
-    FILT_HV       3*24
+    FILT_HV       3*24 ; r0..2; PIC
+    PIC_END ; r3, push/pop
     RET
 
 cglobal %1_h264_qpel4_hv_lowpass_h, 3,4 ; tmp, dst, dstStride
@@ -540,6 +580,9 @@ QPEL4_HV1_LOWPASS_OP avg
 
 %macro QPEL8OR16_HV1_LOWPASS_OP 1
 cglobal %1_h264_qpel8or16_hv1_lowpass_op, 4,4,8 ; src, tmp, srcStride, size
+    %define rpicsave ; safe to push/pop rpic
+    PIC_BEGIN r4
+    CHECK_REG_COLLISION "rpic","r0","r1","r2","r3d"
     movsxdifnidn  r2, r2d
     pxor          m7, m7
     movh          m0, [r0]
@@ -555,7 +598,7 @@ cglobal %1_h264_qpel8or16_hv1_lowpass_op, 4,4,8 ; src, tmp, srcStride, size
     punpcklbw     m2, m7
     punpcklbw     m3, m7
     punpcklbw     m4, m7
-    FILT_HV     0*48
+    FILT_HV     0*48 ; r0..2; PIC
     FILT_HV     1*48
     FILT_HV     2*48
     FILT_HV     3*48
@@ -572,8 +615,9 @@ cglobal %1_h264_qpel8or16_hv1_lowpass_op, 4,4,8 ; src, tmp, srcStride, size
     FILT_HV    12*48
     FILT_HV    13*48
     FILT_HV    14*48
-    FILT_HV    15*48
+    FILT_HV    15*48 ; r0..2; PIC
 .end:
+    PIC_END ; r4, push/pop
     RET
 %endmacro
 
@@ -851,4 +895,4 @@ cglobal %1_h264_qpel16_h_lowpass_l2, 5, 6, 16 ; dst, src, src2, dstStride, src2S
 INIT_XMM ssse3
 QPEL16_H_LOWPASS_L2_OP put
 QPEL16_H_LOWPASS_L2_OP avg
-%endif
+%endif ; ARCH_X86_64
